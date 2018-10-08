7ab8b42296098d90bc56ecc9b25c8891b88f4210
OlenaKulchytska <43315063+OlenaKulchytska@users.noreply.github.com>
2018-10-09 00:02:53 +0300

    Create lab_4.c

---
+++ Lab_4/lab_4.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int n, k, num;
+
+    printf("Enter n: \n");
+    scanf("%d",&n);
+    int mas[n];
+    for(int i=0; i<n; i++)
+    {
+        mas[i]= rand()%100;
+        printf("  %d  ",mas[i]);
+    }
+
+    printf("\nEnter k:\n");
+    scanf("%d",&k);
+
+    num=n-k;
+    for(int j=k; j>0; j--)
+    {
+        printf("  %d  ",mas[j-1]);
+    }
+    for(int l=n; l-1>num; l--)
+    {
+        printf("  %d  ",mas[l-1]);
+    }
+
+    printf("\nNew mas: \n");
+
+    for(int j=k-1; j>0; j--)
+    {
+        printf("  %d  ",mas[j-1]);
+    }
+    for(int l=n; l-1>(num+1); l--)
+    {
+        printf("  %d  ",mas[l-1]);
+    }
+
+    printf("\nAnother one: \n");
+
+    for(int j=k+1; j<n; j++)
+    {
+        printf("  %d  ",mas[j]);
+    }
+    for(int l=0; l<(k-1); l++)
+    {
+        printf("  %d  ",mas[l]);
+    }
+    return 0;
+}
