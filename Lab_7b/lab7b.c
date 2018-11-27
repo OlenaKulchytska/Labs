df30ac337a7fa22eed33343c3cd858ba6df8fc95
OlenaKulchytska <43315063+OlenaKulchytska@users.noreply.github.com>
2018-11-27 22:53:17 +0200

    Update lab7b.c

--- Lab_7b/lab7b.c
+++ Lab_7b/lab7b.c
@@ -1,34 +1,25 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdarg.h>
-
+  
 void OctalToDecimal(int n,...)
 {
-long int decimal=0;
-int M[n];
-va_list list;
-va_start(list,n);
-for (int j = 0; j < n; ++j) {
-M[j] = va_arg(list, int);
+  int M[n];
+  va_list list;
+  va_start(list,n);
+  for (int j = 0; j < n; ++j) {
+     M[j] = va_arg(list, int);
+  }
+  for(int j=0; j<n; j++){
+    printf("\nEquivalent decimal value: %d",M[j]);
+  }
 }
-int j=0;
-for (int i = 0; i < n;i++) {
-while (M[i] != 0)
+
+ int main()
 {
-decimal = decimal +(M[i] % 10)* pow(8, j++);
-M[i] = M[i] / 10;
-}
-printf("\nEquivalent decimal value: %ld",decimal);
-j=0;
-}
-
-}
-int main()
-{
-OctalToDecimal(3,22,55,77);
-OctalToDecimal(6,120,30,23,109,77,86);
-OctalToDecimal(7,99,19,22,299,33,12,85);
-
-
+OctalToDecimal(3,022,055,077);
+OctalToDecimal(6,0120,030,023,0107,077,06);
+OctalToDecimal(7,03,016,022,021,033,012,075);
 return 0;
 }
+
