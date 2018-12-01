348327436057a46488ffaae8581d28fd7aab0424
OlenaKulchytska <43315063+OlenaKulchytska@users.noreply.github.com>
2018-12-01 13:46:03 +0200

    Create lab10.c

---
+++ Lab_10/lab10.c
@@ -0,0 +1,80 @@
+include <stdio.h>
+#include <stdlib.h>
+int **make_mas(int rows,int columns)
+{
+    int **mas;
+    mas=(int)malloc(rows*sizeof(int));
+    for(int i=0;i<rows;i++){
+        mas[i]=(int)malloc(columns*sizeof(int));
+    }
+    for(int i=0;i<rows;i++){
+        for(int j=0; j<columns;j++){
+            mas[i][j]=rand()%200;
+        }
+    }
+    return mas;
+}
+int main()
+{
+    int max=0;
+    int max_r,max_c;
+    int rows,columns;
+    printf("Enter rows:\n");
+    scanf("%d",&rows);
+    printf("Enter columns:\n");
+    scanf("%d",&columns);
+    int **array;
+    array=make_mas(rows, columns);
+
+    for(int i=0;i<rows;i++){
+        for(int j=0; j<columns;j++){
+            printf("%d\t",array[i][j]);
+        }
+        printf("\n");
+    }
+    for(int i=0;i<rows;i++){
+        for(int j=0; j<columns;j++){
+            if(max<array[i][j]) {
+                max=array[i][j];
+                max_r = i;
+                max_c = j;
+            }
+        }
+    }
+    printf("Max element = %d\n",max);
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<columns;j++){
+            if(max_r==i||max_c==j){
+                array[i][j]=-1;
+            }
+        }
+    }
+
+    for(int i=0;i<rows;i++){
+        for(int j=0; j<columns-1;j++){
+            if(array[i][j]==-1){
+                array[i][j]= array[i][j+1];
+                array[i][j+1] = -1;
+            }
+        }
+    }
+
+    for(int i=0;i<rows-1;i++){
+        for(int j=0; j<columns;j++){
+            if(array[i][j]==-1){
+                array[i][j]= array[i+1][j];
+                array[i+1][j] = -1;
+            }
+        }
+    }
+    printf("\n");
+    for(int i=0;i<rows;i++){
+        for(int j=0; j<columns;j++){
+            if(array[i][j] != -1){
+                printf("%d\t",array[i][j]);
+            }
+
+        }
+        printf("\n");
+    }
+}
